e0455df188bacb74d25f8417709ee2ef1271cbd5
Vishal Kalita <vishalkalita036@gmail.com>
2024-11-06 23:47:52 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,9 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", process.env.FRONTEND_URL],
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:5174",],
   credentials: true
 }))
 
@@ -66,6 +67,6 @@ app.get('/api/sheet-data', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(8000, () => {
+  console.log('Server listening on port 8000');
 });
